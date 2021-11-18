4af97c84ed00eb6314b958170d696326daac168e
Tarikul Hasan Shuvo <tarikul15-7944@diu.edu.bd>
2021-11-18 11:24:59 +0600

    add env file

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const ApiService = {
     init() {
-        axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+        axios.defaults.baseURL = process.env.REACT_APP_API;
         axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem('accessToken')}`;
     },
 
